7724fa25520742704fffc307bb718a3abad7da4f
ntando <ntandocyrilnkuna@gmail.com>
2020-02-27 22:26:40 +0200

    added font Open Sens and finish styling menu-item

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './menu-item.styles.scss';
 function MenuItem({ title, imageUrl, size }) {
   return (
-    <div
-      className={`${size} menu-item`}
-      style={{ backgroundImage: `url(${imageUrl}) ` }}
-    >
+    <div className={`${size || ''} menu-item`}>
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl}) ` }}
+      />
       <div className='content'>
-        <h1 className='title'>{title}</h1>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <spa className='subtitle'>SHOP NOW</spa>
       </div>
     </div>
