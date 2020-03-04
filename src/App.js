14f67b4c5866d811e444b604c35d5e974b1c3556
ntando <ntandocyrilnkuna@gmail.com>
2020-03-04 18:16:35 +0200

    finished sign in  handleSubmit method

--- src/App.js
+++ src/App.js
@@ -37,7 +37,6 @@ class App extends React.Component {
     this.unsubscribeFromAuth();
   }
   render() {
-    console.log(this.state);
     return (
       <div>
         <Header currentUser={this.state.currentUser} />
